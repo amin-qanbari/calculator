8013a7944a93713457eec01dadd8696fd2df190b
amin <aminqanbarii1992@gmail.com>
2022-02-25 10:12:59 -0800

    add clear

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ function App() {
 
   //handler click
   const clickHandler = (e) => {
-    setResult(e.target?.name)
+    setResult(result?.concat(e.target?.name))
   }
+    //clear
+    const clear = () => {
+      setResult('')
+    }
+  
+
+
   return (
     <>
       <div className="container">
@@ -15,23 +22,23 @@ function App() {
         <div className="calculator">
           <input type="text" className="calcNumbers" value={result} />
           <div className="buttons">
-            <button className="btn clear span-2">C</button>
-            <button className="btn orange">&larr;</button>
-            <button name="/" className="btn orange">&divide;</button>
+            <button onClick={} className="btn clear span-2">C</button>
+            <button onClick={} className="btn orange">&larr;</button>
+            <button onClick={} name="/" className="btn orange">&divide;</button>
             <button onClick={clickHandler} name="7" className="btn">7</button>
-            <button name="8" className="btn">8</button>
-            <button name="9" className="btn">9</button>
-            <button name="*" className="btn orange">x</button>
-            <button name="4" className="btn">4</button>
-            <button name="5" className="btn">5</button>
-            <button name="6" className="btn">6</button>
-            <button name="-" className="btn">-</button>
-            <button name="1" className="btn">1</button>
-            <button name="2" className="btn">2</button>
-            <button name="3" className="btn">3</button>
-            <button name="+" className="btn orange">+</button>
-            <button name="0" className="btn span-3">0</button>
-            <button className="btn orange equal">=</button>
+            <button onClick={clickHandler} name="8" className="btn">8</button>
+            <button onClick={clickHandler} name="9" className="btn">9</button>
+            <button onClick={clickHandler} name="*" className="btn orange">x</button>
+            <button onClick={clickHandler} name="4" className="btn">4</button>
+            <button onClick={clickHandler} name="5" className="btn">5</button>
+            <button onClick={clickHandler} name="6" className="btn">6</button>
+            <button onClick={clickHandler} name="-" className="btn">-</button>
+            <button onClick={clickHandler} name="1" className="btn">1</button>
+            <button onClick={clickHandler} name="2" className="btn">2</button>
+            <button onClick={clickHandler} name="3" className="btn">3</button>
+            <button onClick={clickHandler} name="+" className="btn orange">+</button>
+            <button onClick={clickHandler} name="0" className="btn span-3">0</button>
+            <button onClick={} className="btn orange equal">=</button>
           </div>
         </div>
       </div>
